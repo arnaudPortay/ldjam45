2dbcbf08133a47f52bb0476ad5fe133c64de724f
Saowll <50049598+Saowll@users.noreply.github.com>
2019-10-05 17:09:25 +0200

    Séparation du code pour le avancer/reculer et la rotation

--- NASA/Assets/Scripts/Mouse_Behaviour.cs
+++ NASA/Assets/Scripts/Mouse_Behaviour.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class Mouse_Behaviour : MonoBehaviour
 {
-    public float speed = 6f;   
+    public float speed = 10f;   
     Vector3 movement;                   // The vector to store the direction of the player's movement.                      // Reference to the animator component.
     Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-    public float rotateSpeed = 0;
 
     void Start()
     {
@@ -19,19 +18,12 @@ public class Mouse_Behaviour : MonoBehaviour
     {
         // Store the input axes.
         float h = Input.GetAxisRaw("Vertical");
-        float y = Input.GetAxisRaw("Horizontal");
         
         if 
             (h !=0)
         {
             // Move the player around the scene.
             Move (h);
-        }  
-        if 
-            (y !=0)
-        {
-            // Change the rotation player
-            TurningToMouse (y);
         }  
     }
 
@@ -46,19 +38,5 @@ public class Mouse_Behaviour : MonoBehaviour
         // Move the player to it's current position plus the movement.
         playerRigidbody.MovePosition (transform.position + movement);
 
-    }
-
-    void TurningToMouse (float h)
-    {
-
-        // Set the movement vector based on the axis input.
-        movement.Set (0f, h, 0f);
-
-        // Normalise the movement vector and make it proportional to the rotate speed per second.
-        movement = movement.normalized * rotateSpeed;
-
-        // Move the player to it's current position plus the movement.
-        Quaternion deltaRotation = Quaternion.Euler(movement * Time.deltaTime);
-        playerRigidbody.MoveRotation(playerRigidbody.rotation * deltaRotation);
     }
 }
---
+++ NASA/Assets/Scripts/Mouse_Rotation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Rotation : Ability
+{
+    Vector3 movement;                   // The vector to store the direction of the player's movement.                      // Reference to the animator component.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    public float rotateSpeed = 200;
+
+     new protected void InitAbility ()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        playerRigidbody = subject.GetComponent<Rigidbody>();
+    }
+
+    protected void FixedUpdate ()
+    {
+        // Store the input axes.
+        float y = Input.GetAxisRaw("Horizontal");
+        if 
+            (y !=0)
+        {
+            // Change the rotation player
+            TurningToMouse (y);
+        }  
+    }
+
+    void TurningToMouse (float h)
+    {
+        if (!playerRigidbody)
+        {
+            InitAbility();
+        }
+
+        // Set the movement vector based on the axis input.
+        movement.Set (0f, h, 0f);
+
+        // Normalise the movement vector and make it proportional to the rotate speed per second.
+        movement = movement.normalized * rotateSpeed;
+
+        // Move the player to it's current position plus the movement.
+        Quaternion deltaRotation = Quaternion.Euler(movement * Time.deltaTime);
+        playerRigidbody.MoveRotation(playerRigidbody.rotation * deltaRotation);
+    }
+}
