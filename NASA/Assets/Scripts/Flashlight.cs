266babfaec302a0db05f55ef8544940ac89f9799
Morgane Fouquet <morganefouquet2@gmail.com>
2019-10-05 23:10:56 +0200

    Changer la couleur de la light

--- NASA/Assets/Scripts/Flashlight.cs
+++ NASA/Assets/Scripts/Flashlight.cs
@@ -2,19 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flashlight : MonoBehaviour
+public class Flashlight : Ability
 {
+    // All available colors
+    Color[] mColors = new Color[5] { Color.white, Color.yellow, Color.red, Color.green, Color.blue};
+
     private Light myLight;
+
+    private int mCurrentColorIndex;
 
     void Start()
     {
-        myLight = GetComponent<Light>();
+        mCurrentColorIndex = 0;
     }
+
+    protected override void InitAbility ()
+    {
+        if
+            (!subject)
+        {
+            return;
+        }
+        myLight = subject.GetComponent<Light>();
+
+        if
+            (myLight)
+        {
+            myLight.color = mColors[mCurrentColorIndex];
+            myLight.enabled = true; 
+        }
+    }
+
     void Update ()
     {
-        if(Input.GetKeyUp(KeyCode.Space))
+        if
+            (!subject)
         {
-            myLight.enabled = !myLight.enabled;
+            return;
+        }
+
+        // Space key to enable the light
+        if
+            (Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.color = mColors[mCurrentColorIndex];
+            myLight.enabled = !myLight.enabled; 
+        }
+
+        // F key to change the color of the light
+        if
+            (Input.GetKeyUp(KeyCode.F))
+        {
+            mCurrentColorIndex = (mCurrentColorIndex + 1) % mColors.Length;
+            myLight.color = mColors[mCurrentColorIndex];
         }
     }
 }
