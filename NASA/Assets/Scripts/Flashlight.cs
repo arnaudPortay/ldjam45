d4f1cf5c4d08609c1fc6eeaa91fdc7da195751c9
Brainstorm67 <Brainstorm67@users.noreply.github.com>
2019-10-05 23:14:52 +0200

    update flashlight récupérable + scene principale

--- NASA/Assets/Scripts/Flashlight.cs
+++ NASA/Assets/Scripts/Flashlight.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
-public class Flashlight : MonoBehaviour
+public class Flashlight : Ability
 {
     private Light myLight;
-
+     bool Toggleable = true;
     void Start()
     {
+        
+    }
+
+    protected override void InitAbility ()
+    {
+        //print("initialized");
         myLight = GetComponent<Light>();
     }
-    void Update ()
+    private async Task delayedWork()
     {
-        if(Input.GetKeyUp(KeyCode.Space))
+        await Task.Delay(200);
+        Toggleable = true;
+    }
+    void FixedUpdate ()
+    {
+        if(myLight && Toggleable && Input.GetKey(KeyCode.R))
         {
+            Toggleable = false;
+            //print("toggled");
             myLight.enabled = !myLight.enabled;
+            this.delayedWork();
         }
     }
 }
