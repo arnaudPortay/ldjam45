686912425d16a9ff40f41e688a659fbfcccdf10e
Saowll <50049598+Saowll@users.noreply.github.com>
2019-10-05 18:10:39 +0200

    Ajout de torche/lumières

---
+++ NASA/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private Light myLight;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+    void Update ()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
