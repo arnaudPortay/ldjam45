7a95beb7a9614c07532c778bbd78dbc762424d4d
Morgane Fouquet <morganefouquet2@gmail.com>
2019-10-06 15:52:41 +0200

    Ajout possible du blur quand on arrive dans le jeu

--- NASA/Assets/Scripts/GameEngine.cs
+++ NASA/Assets/Scripts/GameEngine.cs
@@ -9,6 +9,14 @@ public class GameEngine : MonoBehaviour
 
     public Mouse_Behaviour mouseBehaviour;
 
+    private CameraEffect cameraEffect;
+
+    private Material blurMaterial;
+
+    public float blurInitValue = 0.10f;
+
+    public float blurDecrementStep = 0.01f;
+
     private bool isInit = false;
     
     // Start is called before the first frame update
@@ -18,6 +26,15 @@ public class GameEngine : MonoBehaviour
         {
             player.SetActive(false);
         }
+
+        cameraEffect = Camera.main.GetComponent<CameraEffect>();
+
+        if
+            (cameraEffect)
+        {
+            blurMaterial = cameraEffect.postprocessMaterial;  
+        }
+         
     }
 
     void initGame()
@@ -39,7 +56,34 @@ public class GameEngine : MonoBehaviour
     {
         if (!bootScreen.activeSelf && !isInit)
         {
+            if
+                (blurMaterial && cameraEffect)
+            {
+                blurMaterial.SetFloat ("_BlurSize", blurInitValue);
+                cameraEffect.enabled = true;
+            }
+            
             initGame();
+        }
+
+        if
+            (isInit && cameraEffect && blurMaterial && cameraEffect.enabled)
+        {
+            if
+                (blurMaterial.GetFloat ("_BlurSize") > 0)
+            {
+                if
+                    (blurMaterial.GetFloat ("_BlurSize") - blurDecrementStep > 0)
+                {
+                    blurMaterial.SetFloat ("_BlurSize", blurMaterial.GetFloat ("_BlurSize") - blurDecrementStep);
+                }
+                else
+                {
+                    blurMaterial.SetFloat ("_BlurSize", 0f);
+                    cameraEffect.enabled = false;
+                }
+                
+            }
         }
     }
 }
