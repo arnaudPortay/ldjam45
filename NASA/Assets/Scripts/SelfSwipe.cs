666fd0271d231650a3c1e891049753e727e106a9
Brainstorm67 <Brainstorm67@users.noreply.github.com>
2019-10-06 20:47:32 +0200

    intégration de la nouvelle mouse

---
+++ NASA/Assets/Scripts/SelfSwipe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Self rotation for objects to pick up.
+ */
+
+public class SelfSwipe : MonoBehaviour {
+    public float rotationSpeed = 300;
+    public Vector3 RotationAxis = new Vector3(0,1,0);
+    void Update () 
+    {
+        // TODO : desactivate when the object is picked up
+        Vector3 move = RotationAxis*rotationSpeed;
+        // Set the movement vector based on the axis input.
+        transform.Rotate (move * Time.deltaTime);
+        Vector3 angles = transform.eulerAngles;
+        print("angles :"+angles);
+    }
+}
